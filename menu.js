31297e015e63c25b765bac9f5696b95018bd881c
Alistyler <ali.hamid9119@gmail.com>
2025-01-19 02:20:40 +0100

    Ist noch nicht fertig!

--- menu.js
+++ menu.js
@@ -23,7 +23,36 @@ circle.appendChild(burger);
 container.appendChild(circle);
 nav.appendChild(container);
 
+let menuList = document.createElement('ul');
+menuList.className = "menu-list";
+
+const menuItems = [
+  "Terminplaner",
+  "Lernbereich",
+  "Training",
+  "Ernährungsbereich",
+  "Alltagsversorgung und Sicherheit",
+  "Kommunikation",
+  "Einstellungen"
+];
+
+menuItems.forEach(item => {
+  let li = document.createElement('li');
+  li.textContent = item;
+  menuList.appendChild(li);
+});
+
+container.appendChild(menuList);
+
 let header = document.querySelector('header');
 header.appendChild(nav);
 
-console.log(header);
+burger.addEventListener('click', () => {
+    menuList.classList.toggle('active');
+  
+    // Animation für das Burger-Menü
+    spanUp.classList.toggle('rotate-up');
+    spanMid.classList.toggle('fade-out');
+    spanDown.classList.toggle('rotate-down');
+  });
+  
